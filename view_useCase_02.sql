19d5bc923db20fac67ac0913e2c0985ed1542b9a
krishna cheashty <krishnacheashty@gmail.com>
2026-03-15 05:49:02 +0600

    add more table attribute by joining tables and make a view table

---
+++ view_useCase_02.sql
@@ -0,0 +1,44 @@
+--task : provide view that combine details form orders,products,customer, and employees.
+
+/*
+SELECT 
+O.OrderID,
+O.OrderDate,
+O.Sales,
+O.Quantity,
+P.Product,
+P.Category,
+COALESCE(C.FirstName,'') + ' ' +COALESCE(C.LastName,'') AS CustomerFullName,
+C.Country CustomerCountry,
+COALESCE(E.FirstName,'') + ' ' +COALESCE(E.LastName,'') AS SalesFullName
+FROM Sales.Orders AS O
+LEFT JOIN Sales.Products AS P
+ON P.ProductID=O.ProductID
+LEFT JOIN Sales.Customers AS C
+ON C.CustomerID=O.CustomerID
+*/
+CREATE VIEW Sales.v_Order_Details as (
+	SELECT
+		O.OrderID,
+		O.OrderDate,
+		O.Sales,
+		O.Quantity,
+		P.Product,
+		P.Category,
+		COALESCE(C.FirstName,'') + ' ' + COALESCE(c.LastName,'') CustomerName,
+		C.Country,
+		COALESCE(e.FirstName,'') + ' ' + COALESCE(e.LastName,'') SalerName,
+		e.Department
+	FROM Sales.Orders O
+	LEFT JOIN Sales.Products P
+	ON P.ProductID=O.ProductID
+	LEFT JOIN Sales.Customers c
+	ON c.CustomerID=O.CustomerID
+	LEFT JOIN Sales.Employees AS e
+	ON e.EmployeeID=O.SalesPersonID
+)
+GO
+
+SELECT 
+*
+FROM Sales.v_Order_Details
